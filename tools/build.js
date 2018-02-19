7aca9cfe73bd759ba8b2401a7ad92fc3962f4a37
Clément Nerma <clement.nerma@gmail.com>
2018-02-19 10:39:17 +0100

    Set up a list of modules + checking

--- tools/build.js
+++ tools/build.js
@@ -15,7 +15,31 @@ function error (message, errcode = 1) {
 }
 
 // Load some Yarn modules
-const chalk = require('chalk');
+const chalk = require('chalk'),
+      minimist = require('minimist');
+
+// Set up a list of modules with their respective folders
+const modules = {
+  // Module's slug (used in the CLI) associated to its purpose
+  // This slug is also the module's filename (without its extension)
+  book: 'Build the books'
+};
+
+// Parse the command-line arguments
+const argv = minimist(process.argv.slice(2) /* Ignore Node.js call arguments */);
+
+// Get the module's name to run
+const modname = argv.module || argv._[0];
+
+// If no module was specified...
+if (typeof modname !== 'string')
+  // ERROR
+  error('No build module specified', 2);
+
+// If the specified module is unknown...
+if (! modules.hasOwnProperty(modname))
+  // ERROR
+  error(`Unknown build module "${modname}"`, 3);
 
 // Exit with an error
 error('Build tools are not ready yet');
