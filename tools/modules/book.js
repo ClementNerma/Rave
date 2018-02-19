308abcae8bc3455149b490defe068af729025100
Clément Nerma <clement.nerma@gmail.com>
2018-02-19 20:36:57 +0100

    Parse and structure the books' source

--- tools/modules/book.js
+++ tools/modules/book.js
@@ -72,5 +72,125 @@ self = {
 
     // Load the "markdown-it" module
     const mdIt = require('markdown-it')({});
+
+    // Is a section opened?
+    let opened = false;
+
+    // Generate the sections
+    let sections = [];
+
+    // The section buffer
+    let sectionBuff = [];
+
+    // The title buffer
+    let titleBuff = [];
+
+    // The main title
+    let mainTitle;
+
+    // The last title's slug
+    let lastSlug = '';
+
+    // Make a counter for each level of title
+    let counter = [0, 0, 0, 0, 0];
+
+    // Is this the first section?
+    let firstSection = true;
+
+    // For each line in the source code...
+    for (let line of source.split(/\r\n|\r|\n/g).concat('## END OF FILE') /* To close the last section */) {
+      // If this is a title...
+      if (line.startsWith('#')) {
+        // Use a RegExp to extract informations from it
+        const match = line.match(/^(#+) *(.*)$/);
+
+        // Get the title's depth
+        const depth = match[1].length;
+
+        // Get the title's content
+        const title = match[2];
+
+        // Generate a slug for this title
+        const slug = title.toLocaleLowerCase()
+          // Replace spaces by dashes
+          .replace(/ /g, '-')
+          // Remove special characters
+          .replace(/[^a-z0-9-_]/g, '');
+
+        // If this is not the main title
+        // and if the buffer is not empty...
+        if (depth >= 2) {
+          // Generate the HTML associated to the buffer
+          const html = mdIt.render(titleBuff.join('\n'));
+
+          // If the HTML is not empty...
+          if (html.trim())
+            // Push the title buffer the output
+            sectionBuff.push({
+              _CONTENT: true,
+              html
+            });
+
+          // Reset the section buffer
+          titleBuff = [];
+        }
+
+        // If this is a section's title
+        if (depth === 2) {
+          // If this is the first one...
+          if (firstSection)
+            // Remove the "first section" marker
+            firstSection = false;
+          // Else...
+          else {
+            // Push the section buffer to the output
+            sections.push({
+              slug: lastSlug,
+              content: sectionBuff
+            });
+
+            // Reset the section buffer
+            sectionBuff = [];
+          }
+
+          // Remember this title's slug
+          lastSlug = slug;
+        }
+
+        // If this is NOT the main title...
+        if (depth !== 1) {
+          // Increase the counter for this level of title
+          counter[depth] ++;
+
+          // For every counter with a level higher than this one...
+          for (let i = depth + 1; i < counter.length; i++)
+            // Reset the counter
+            counter[i] = 0;
+        }
+
+        // Make the title's object
+        let title_obj = {
+          _TITLE: true,
+          text: mdIt.renderInline(title), // Without the "<p>...</p>" wrapping
+          slug,
+          depth,
+          depth_dec: depth - 1,
+          prettydepth: (depth === 1) ? '0.' : counter.filter(c => c > 0).join('.') + '.'
+        };
+
+        // If this is the main title...
+        if (depth === 1)
+          // Set it as the main title's object
+          mainTitle = title_obj;
+        else
+          // Push it into the section buffer
+          sectionBuff.push(title_obj);
+      }
+
+      // Else...
+      else
+        // Push it to the buffer
+        titleBuff.push(line);
+    }
   }
 };
