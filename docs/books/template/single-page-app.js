671c1adcf72668edbe6ff783a141ba7372adb358
Clément Nerma <clement.nerma@gmail.com>
2018-02-19 21:16:13 +0100

    Make an empty script for book's template

---
+++ docs/books/template/single-page-app.js
@@ -0,0 +1,8 @@
+/**
+ * @file This file modified the book's HTML page by showing only one section at a time and making a dynamic summary
+ * @author Clément Nerma
+ * @license Apache-2.0
+ */
+
+// Enable strict mode
+"use strict";
