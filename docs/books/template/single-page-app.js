fb6fac990b83849ebd161ed35781aa71bc02bfbe
Clément Nerma <clement.nerma@gmail.com>
2018-02-19 21:16:30 +0100

    Make API for DOM elements selection in template

--- docs/books/template/single-page-app.js
+++ docs/books/template/single-page-app.js
@@ -6,3 +6,23 @@
 
 // Enable strict mode
 "use strict";
+
+/**
+ * Get an element using a query selector
+ * @param {string} selector The selector
+ * @returns {HTMLElement|null} A DOM element
+ */
+function q(selector) {
+  // Get the element and return it
+  return document.querySelector(selector);
+}
+
+/**
+ * Make an array from a query selector's output
+ * @param {string} selector The selector
+ * @returns {Array<HTMLElement>} An HTML array
+ */
+function qa(selector) {
+  // Make the query, convert its result to an array and return it
+  return Array.from(document.querySelectorAll(selector));
+}
