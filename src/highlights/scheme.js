074ade95764e0001df12726faddca21299531941
Clément Nerma <clement.nerma@gmail.com>
2018-02-20 11:11:38 +0100

    Add syntax constants

--- src/highlights/scheme.js
+++ src/highlights/scheme.js
@@ -4,6 +4,28 @@
 // Export the scheme
 scheme = {
   constants: {
+    // Define build constants
+    LANGUAGE: 'SilverNight',
+    LOWERCASE_LANGUAGE: 'silvernight',
+    EXTENSION: 'sn',
+    VERSION: '0.8.2',
+    LICENSE: 'MIT',
+    REPOSITORY_TYPE: 'git',
+    REPOSITORY_URL: 'https://github.com/ClementNerma/SilverNight',
+    README: "# SilverNight\n\nSyntax highlighting package for the [SilverNight language](https://github.com/ClementNerma/SilverNight).",
+
+    // Define all native colors
+    blue: 'entity.name.function.sn',
+    cyan: 'support.function.sn',
+    gray: 'comment.block.sn.sn',
+    green: 'entity.other.inherited-class.sn',
+    orange: 'constant.other.sn',
+    purple: 'storage.type.var.sn',
+    red: 'variable.language.sn',
+    white: 'unknown.must.be.white.sn',
+    yellow: 'entity.name.type.class.sn',
+    invalid: 'invalid.illegal.sn',
+
     // Visual Studio Code specific variables
     execPath: '${execPath}',
     workspaceRoot: '${workspaceRoot}'
