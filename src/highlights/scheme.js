e742126ecb939605bc2f8e9236999e502931f12d
Clément Nerma <clement.nerma@gmail.com>
2018-02-20 11:11:20 +0100

    Make an empty syntax scheme file

---
+++ src/highlights/scheme.js
@@ -0,0 +1,13 @@
+// Enable strict mode
+"use strict";
+
+// Export the scheme
+scheme = {
+  constants: {
+    // Visual Studio Code specific variables
+    execPath: '${execPath}',
+    workspaceRoot: '${workspaceRoot}'
+  },
+
+  patterns: []
+};
