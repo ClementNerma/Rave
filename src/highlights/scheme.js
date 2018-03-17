3821c43ba70609e543f57867ee97429f136459f2
Clément Nerma <clement.nerma@gmail.com>
2018-03-17 14:54:07 +0100

    Add syntax rule: '#reversable' directive

--- src/highlights/scheme.js
+++ src/highlights/scheme.js
@@ -8,7 +8,7 @@ scheme = {
     LANGUAGE: 'SilverNight',
     LOWERCASE_LANGUAGE: 'silvernight',
     EXTENSION: 'sn',
-    VERSION: '0.10.1',
+    VERSION: '0.10.2',
     LICENSE: 'MIT',
     REPOSITORY_TYPE: 'git',
     REPOSITORY_URL: 'https://github.com/ClementNerma/SilverNight',
@@ -514,7 +514,7 @@ scheme = {
     ],
     [
       // Generic known directives
-      /#(Dynamic|alias|samedef|bind|makebindings|declarative|auto|declare|process|ref|raw_indent|if|else|end|plain|only)( +;)?/,
+      /#(Dynamic|alias|samedef|bind|makebindings|declarative|auto|declare|process|ref|raw_indent|if|else|end|plain|only|reversable)( +;)?/,
       'orange'
     ],
     [
