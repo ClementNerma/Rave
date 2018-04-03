09e61e432a7c64a7e8f56eae5d56dc1d9b664df7
Clément Nerma <clement.nerma@gmail.com>
2018-04-03 11:51:06 +0200

    Update syntax rule: new return type symbol (arrow)

--- src/highlights/_scheme.js
+++ src/highlights/_scheme.js
@@ -8,7 +8,7 @@ scheme = {
     LANGUAGE: 'SilverNight',
     LOWERCASE_LANGUAGE: 'silvernight',
     EXTENSION: 'sn',
-    VERSION: '0.16.2',
+    VERSION: '0.17.0',
     LICENSE: 'MIT',
     REPOSITORY_TYPE: 'git',
     REPOSITORY_URL: 'https://github.com/ClementNerma/SilverNight',
@@ -315,13 +315,13 @@ scheme = {
     ],
     [
       // Function's or declaration's special type (not caught by the previous expressions)
-      /(?<!:)(:)\s*((?:&\s*)*)(void|self|Any|class_ref|func_ref|var_ref|macro_ref|lambda|T|X|Y|Z|K|V)(\?)?(?=\s*[\{\[\),;=]|\s*=>|\s*with)/,
-      'white', 'cyan', 'purple', 'cyan'
+      /(?<!:)(?:(:)|(->))\s*((?:&\s*)*)(void|self|Any|class_ref|func_ref|var_ref|macro_ref|lambda|T|X|Y|Z|K|V)(\?)?(?=\s*[\{\[\),;=]|\s*=>|\s*with)/,
+      'white', 'purple', 'cyan', 'purple', 'cyan'
     ],
     [
       // Function's or declaration's (other) type
-      /(?<!:)(:)\s*((?:&\s*)*)([a-zA-Z_\$][a-zA-Z0-9_\$]*)(\?)?(?=\s*[\{\[\),;=]|\s*=>|\s*with)/,
-      'white', 'cyan', 'yellow', 'cyan'
+      /(?<!:)(?:(:)|(->))\s*((?:&\s*)*)([a-zA-Z_\$][a-zA-Z0-9_\$]*)(\?)?(?=\s*[\{\[\),;=]|\s*=>|\s*with)/,
+      'white', 'purple', 'cyan', 'yellow', 'cyan'
     ],
     [
       // Arrow function
