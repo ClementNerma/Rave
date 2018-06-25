30f9e9dc4bed2e27dc7b67b6cf118fd53163db9c
Clément Nerma <clement.nerma@gmail.com>
2018-06-25 17:18:55 +0200

    Add syntax rule: 'overload' object

--- src/highlights/_scheme.js
+++ src/highlights/_scheme.js
@@ -15,7 +15,7 @@ scheme = {
     LANGUAGE: 'SilverNight',
     LOWERCASE_LANGUAGE: 'silvernight',
     EXTENSION: 'sn',
-    VERSION: '0.25.5',
+    VERSION: '0.25.6',
     LICENSE: 'MIT',
     REPOSITORY_TYPE: 'git',
     REPOSITORY_URL: 'https://github.com/ClementNerma/SilverNight-draft',
@@ -277,6 +277,11 @@ scheme = {
         [
           // Static context operator
           /\bstatic(?= *<)/,
+          'purple'
+        ],
+        [
+          // Overload context operator
+          /\boverload(?= *<)/,
           'purple'
         ],
         [
