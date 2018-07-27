2f83947e6b8488d2f06607293f6d6635e8ee9d51
Clément Nerma <clement.nerma@gmail.com>
2018-07-27 22:57:20 +0200

    Add syntax rule: new 'parse_string' magic directive

--- src/editors-extension/_scheme.js
+++ src/editors-extension/_scheme.js
@@ -769,7 +769,7 @@ scheme = {
         ],
         [
           // Magic callable directives
-          /#__magic_(?:iterate_tuple|clone|serialize|unserialize|cast|cast_unsafe|proxy|flex_proxy|is_same|import|reference_level|arguments|return_type|set_primitive|random_primitive)/,
+          /#__magic_(?:iterate_tuple|clone|serialize|unserialize|cast|cast_unsafe|proxy|flex_proxy|is_same|import|reference_level|arguments|return_type|set_primitive|random_primitive|parse_string)/,
           'orange'
         ],
         [
