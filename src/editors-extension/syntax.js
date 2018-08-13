68c08c2965f2e2b85377579a3e6d0d0735ea0018
Clément Nerma <clement.nerma@gmail.com>
2018-08-13 11:26:14 +0200

    Add syntax rule: 'keyof' operator

--- src/editors-extension/syntax.js
+++ src/editors-extension/syntax.js
@@ -625,7 +625,7 @@ SYNTAX = {
         ],
         [
           // Keywords
-          /(?<!\.)\b(func|lambda|do|if|ift|else|elsif|for|while|loop|unless|until|break|continue|match|default|try|catch|finally|segment|delete|mut|let|val|pln|proxy\!?|public|protected|private|friend|static|abstract|final|unique|virtual|open|extern|readonly|in|export|return|async|iter|yield|flex|await|sync|resolve|reject|throw|with|new|include|namespace|struct|enum|class|type|dict|trait|interface|implements|throws|use|uses|inherits|parentof|instanceof|instanceofsuper|scope|import|from|as)(?!\s*:)\b/,
+          /(?<!\.)\b(func|lambda|do|if|ift|else|elsif|for|while|loop|unless|until|break|continue|match|default|try|catch|finally|segment|delete|mut|let|val|pln|proxy\!?|public|protected|private|friend|static|abstract|final|unique|virtual|open|extern|readonly|in|keyof|export|return|async|iter|yield|flex|await|sync|resolve|reject|throw|with|new|include|namespace|struct|enum|class|type|dict|trait|interface|implements|throws|use|uses|inherits|parentof|instanceof|instanceofsuper|scope|import|from|as)(?!\s*:)\b/,
           'purple'
         ],
         [
